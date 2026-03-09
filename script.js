409047620c4abc59dcf1f3ac9ff9aa7420962a3c
Jenny Gonçalves <Jennyjennyk17890d>
2026-03-09 18:57:57 -0300

    projeto sol e lua

---
+++ script.js
@@ -0,0 +1,11 @@
+ /*let e para criar uma variavel*/
+        let hora = new Date().getHours();
+        if(hora >= 6 && hora < 18){
+            document.write("Celestia governa agora ☀️");
+        } else {
+            document.write("Luna governa agora 🌙");
+
+        }
+     
+    
+     
