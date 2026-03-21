546a9f5a78f697c93f5d55eeaec5d38747ff6805
Jenny Gonçalves <Jennyjennyk17890d>
2026-03-21 16:35:50 -0300

    organizacao

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
  /*let e para criar uma variavel*/
         let hora = new Date().getHours();
-        if(hora >= 6 && hora < 18){
-            document.write("Celestia governa agora ☀️");
-        } else {
-            document.write("Luna governa agora 🌙");
+        let body =document.body;
+        let personagem1 = document.getElementById("ponei1")
+        let personagem2 = document.getElementById("ponei2")
+        let sol = document.getElementById("sol")
+        let lua = document.getElementById("lua")
+        
+        if(hora >= 6 && hora < 11){
+            document.body.className = "dia";
+            personagem1.src= "img/Celestia.gif"
 
+
+           personagem1.style.display="block";
+           personagem2.style.display="none";
+           sol.style.display="block";
+           lua.style.display="none";
+
+        } else if(hora >= 12 && hora < 18){
+           document.body.className = "tarde";
+           personagem1.src="img/Celestia.gif"
+
+           personagem1.style.display="block";
+           personagem2.style.display="none";
+
+           sol.style.display="block";
+           lua.style.display="none";
+        
+
+        } else{
+          document.body.className = "noite";
+          personagem2.src= "img/Luna.gif"
+
+          personagem2.style.display="block";
+          personagem1.style.display="none";
+
+          lua.style.display="block";
+          sol.style.display="none";
         }
+
      
     
      
