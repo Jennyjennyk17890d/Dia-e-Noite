eaa0707835b8d747cf7e87bf7174461a0317d3e5
Jennyjennyk17890d <jennifergoncalves780@gmail.com>
2026-03-31 23:12:56 -0300

    Sistema de dia e noite de Equestria

--- script.js
+++ script.js
@@ -5,10 +5,18 @@
         let personagem2 = document.getElementById("ponei2")
         let sol = document.getElementById("sol")
         let lua = document.getElementById("lua")
+        let castelo =document.getElementById("castelo")
+        let estrela =document.getElementById("star")
+        let ceu = document.getElementById("sky")
         
         if(hora >= 6 && hora < 11){
             document.body.className = "dia";
             personagem1.src= "img/Celestia.gif"
+            castelo.style.display= "block"
+            estrela.style.display="none"
+            ceu.style.display="none"
+         
+
 
 
            personagem1.style.display="block";
@@ -19,23 +27,29 @@
         } else if(hora >= 12 && hora < 18){
            document.body.className = "tarde";
            personagem1.src="img/Celestia.gif"
+            castelo.style.display= "block"
+            ceu.style.display="block"
 
            personagem1.style.display="block";
            personagem2.style.display="none";
 
            sol.style.display="block";
            lua.style.display="none";
+           estrela.style.display="none"
         
 
         } else{
           document.body.className = "noite";
+          castelo.style.display="block"
           personagem2.src= "img/Luna.gif"
+          ceu.style.display="none"
 
           personagem2.style.display="block";
           personagem1.style.display="none";
 
           lua.style.display="block";
           sol.style.display="none";
+          estrela.style.display="block";
         }
 
      
